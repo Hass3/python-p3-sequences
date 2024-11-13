dd65cc1c0d2b77f0deaa0b58f0e24f8c162d7e5e
Hass <hassmroueh@gmail.com>
2024-11-12 20:19:26 -0500

    sequences lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+      fibonacci = [0]
+    else:
+       fibonacci=[0,1]
+       for i in range(2,length):
+          next_number = fibonacci[-1] + fibonacci[-2]
+          fibonacci.append(next_number)
+    print(fibonacci)
+
+    
+
+
